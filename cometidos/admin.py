103d72d6cf50f2393722d7df879b0c912865b1d1
RobertoEsteban <roberto.uach@gmail.com>
2016-02-12 18:59:01 -0300

    Se agrega parametros de las persona cuando se crea un cometido

--- cometidos/admin.py
+++ cometidos/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
 from cometidos.models import *
+from personas.models import *
+from territorios.models import *
+from django.db import models
 # Register your models here.
 
-admin.site.register(Cometido)
+class CometidoAdmin(admin.ModelAdmin):
+	list_display = ('rut','nombre')
+
+	def save_model(self, request, obj, form, change):
+		if not change: 
+			persona = Persona.objects.get(pk=request.user)
+			obj.rut = persona.rut
+			obj.grado = persona.grado
+			obj.escalafon = persona.escalafon
+			obj.nombre = request.user.first_name + ' ' + request.user.last_name
+			obj.persona = request.user
+			obj.unidad = persona.unidad
+			obj.region = persona.region
+			obj.save()
+
+	def get_readonly_fields(self, request, obj=None):
+		if obj: # editing an existing object
+			return self.readonly_fields + ('rut', 'nombre','grado','escalafon','unidad','region')
+		return self.readonly_fields
+
+admin.site.register(Cometido,CometidoAdmin)
--- cometidos/models.py
+++ cometidos/models.py
@@ -1,7 +1,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+from personas.models import *
+from territorios.models import *
 
 # Create your models here.
 class Cometido(models.Model):
-	desde = models.DateField(blank=False,null=False)
+	rut = models.CharField("Rut",max_length=12,blank=False,null=False,editable=False)
+	nombre = models.CharField("Nombre Completo", max_length=180, blank=False, null=False,editable=False)
+	persona = models.ForeignKey(User, default=User,editable=False)
+	grado = models.CharField("Grado",max_length=60,blank=False,null=False,editable=False)
+	escalafon = models.CharField("Escalafon",max_length=60,blank=False,null=False,editable=False)
+	unidad = models.CharField("Unidad",max_length=60,blank=False,null=False,editable=False)
+	region = models.CharField("Region",max_length=60,blank=False,null=False,editable=False)
