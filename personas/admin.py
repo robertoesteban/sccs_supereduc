ba78c9e7204edb561037ca4656caf5327d2d1dea
RobertoEsteban <roberto.uach@gmail.com>
2016-02-11 22:37:24 -0300

    Implementacion clase Grados dentro de modelo Personas

--- personas/admin.py
+++ personas/admin.py
@@ -3,3 +3,4 @@ from personas.models import *
 # Register your models here.
 
 admin.site.register(Persona)
+admin.site.register(Grado)
--- personas/models.py
+++ personas/models.py
@@ -2,7 +2,25 @@ from __future__ import unicode_literals
 
 from django.db import models
 from localflavor.cl.forms import CLRutField
+from django.utils import timezone
 # Create your models here.
+
+OPCIONES_GENERO = (
+	('H','Hombre'),
+	('M','Mujer'),
+	)
+
+
+class Grado(models.Model):
+	numero = models.PositiveIntegerField()
+
+	def __unicode__(self):
+		return str(self.numero)
+
+	class Meta:
+		ordering = ['numero']
+		verbose_name_plural = 'Grados'
+
 
 class RutField(models.CharField):
         def __init__(self, *args, **kwargs):
@@ -21,9 +39,11 @@ class Persona(models.Model):
 	nombres = models.CharField("Nombres", max_length=60, blank=False, null=False)
 	paterno = models.CharField("Apellido Paterno", max_length=60, blank=False, null=False)
 	materno = models.CharField("Apellido Materno", max_length=60, blank=False, null=False)
-	updated = models.DateTimeField("Actualizado", auto_now=True, auto_now_add=False)
-        timestamp = models.DateTimeField(auto_now = False, auto_now_add=True)
+	genero = models.CharField("Genero", max_length=1, choices=OPCIONES_GENERO)
 	correo = models.EmailField("Correo Electronico",unique=True, blank=False, null=False)
+	grado = models.ForeignKey("Grado", Grado)
+	updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+        timestamp = models.DateTimeField(auto_now = False, auto_now_add=True)
 
 	def __str__(self):
 		return self.nombres
@@ -34,6 +54,3 @@ class Persona(models.Model):
 	class Meta:
 		ordering = ['nombres']
 		verbose_name_plural = 'Personas'
-
-
-
